1655c7655d20312ad70ce7cde822d718e69a7e68
jjhhoojj156-cmd <jjhhoojj156@gmail.com>
2025-12-28 12:02:44 +0330

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="bg-gray text-gold p-10">
-      <h1 className="text-3xl font-bold">نتایج والیبال</h1>
-      <p>به سایت رسمی نتایج مسابقات خوش آمدید.</p>
-    </main>
-  );
-}
